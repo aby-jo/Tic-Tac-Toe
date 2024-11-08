1a803f82d1faa020f1dcdcc3e898f597241bf754
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-11-08 20:07:57 +0530

    Completed the Tic Tac Toe

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
-function createArray(){
-    array=[[],[],[]]
-    return array
+const body = () => {
+    let array, win, count, gameover;
 
-}
-function tick(a,b){
-    arr=createArray()
-    arr[a][b]=1
+    function createArray() {
+        array = [[null, null, null], [null, null, null], [null, null, null]];
+        return array;
+    }
 
-}
-function check(array_to_check){
-    if(array_to_check[0][0]==array_to_check[0][1]==array_to_check[0][2]){
-        console.log("Win")
+    function tick(a, b, checksum) {
+        array[a][b] = checksum;
     }
-    else if(array_to_check[0][0]==array_to_check[1][1]==array_to_check[2][2]){
-        console.log("Win")
-    }
-    else if(array_to_check[0][0]==array_to_check[1][0]==array_to_check[2][0]){
-        console.log("Win")
-    }
-    else if(array_to_check[1][0]==array_to_check[1][1]==array_to_check[1][2]){
-        console.log("Win")
-    }
-    else if(array_to_check[2][0]==array_to_check[2][1]==array_to_check[2][2]){
-        console.log("Win")
-    }
-    else if(array_to_check[2][0]==array_to_check[1][1]==array_to_check[0][2]){
-        console.log("Win")
-    }
-    else if(array_to_check[0][1]==array_to_check[1][1]==array_to_check[2][1]){
-        console.log("Win")
-    }
-    else if(array_to_check[0][2]==array_to_check[1][2]==array_to_check[2][2]){
-        console.log("Win")
-    }
-    else{
-        console.log("Lose")
-    }
-}
-tick(0,1)
-tick(0,0)
-tick(1,2)
-check(arr)
 
+    function check(array_to_check, checksum) {
+        win = null;
+
+        if (
+            array_to_check[0][0] === checksum && array_to_check[0][1] === checksum && array_to_check[0][2] === checksum ||
+            array_to_check[1][0] === checksum && array_to_check[1][1] === checksum && array_to_check[1][2] === checksum ||
+            array_to_check[2][0] === checksum && array_to_check[2][1] === checksum && array_to_check[2][2] === checksum ||
+            array_to_check[0][0] === checksum && array_to_check[1][0] === checksum && array_to_check[2][0] === checksum ||
+            array_to_check[0][1] === checksum && array_to_check[1][1] === checksum && array_to_check[2][1] === checksum ||
+            array_to_check[0][2] === checksum && array_to_check[1][2] === checksum && array_to_check[2][2] === checksum ||
+            array_to_check[0][0] === checksum && array_to_check[1][1] === checksum && array_to_check[2][2] === checksum ||
+            array_to_check[2][0] === checksum && array_to_check[1][1] === checksum && array_to_check[0][2] === checksum
+        ) {
+            win = true;
+        }
+    }
+    function listen() {
+        createArray();
+        count = 0;
+        gameover = false;
+
+        const box = document.querySelectorAll(".box");
+        box.forEach((boxElement) => {
+            boxElement.addEventListener("click", (event) => {
+                if (gameover) return;
+
+                const id = event.target.id;
+                const num = id.split("");
+                const a = parseInt(num[0], 10);
+                const b = parseInt(num[1], 10);
+
+                let checksum = count === 0 ? 1 : 0;
+                tick(a, b, checksum);
+                check(array, checksum);
+
+                boxElement.textContent = checksum === 1 ? "X" : "O";
+                if (win === true) {
+                    gameover = true;
+                    alert(checksum === 1 ? "Player 1 (X) wins!" : "Player 2 (O) wins!");
+                }
+
+                count = 1 - count;
+            }, { once: true });
+        });
+    }
+
+    listen();
+};
+
+body();
